871afb0772e45de80628390a5c3931913352d898
Kevin Wahome <wahome@talicraft.com>
2017-04-26 11:41:59 +0300

    Added basic splash screen layout.

--- IBM.Interview.ProjectA/Resources/Resource.Designer.cs
+++ IBM.Interview.ProjectA/Resources/Resource.Designer.cs
@@ -108,6 +108,15 @@ namespace IBM.Interview.ProjectA
 			// aapt resource value: 0x7f040001
 			public const int MvxBindingTagUnique = 2130968577;
 			
+			// aapt resource value: 0x7f040002
+			public const int textView1 = 2130968578;
+			
+			// aapt resource value: 0x7f040003
+			public const int textView2 = 2130968579;
+			
+			// aapt resource value: 0x7f040004
+			public const int textView3 = 2130968580;
+			
 			static Id()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
