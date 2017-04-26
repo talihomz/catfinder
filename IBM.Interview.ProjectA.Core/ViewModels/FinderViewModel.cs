25796bf88fbb9c60cad617d18e1f6df0f127c1e5
Kevin Wahome <wahome@talicraft.com>
2017-04-26 17:04:13 +0300

    Implemented picture taking and choosing from gallery.

--- IBM.Interview.ProjectA.Core/ViewModels/FinderViewModel.cs
+++ IBM.Interview.ProjectA.Core/ViewModels/FinderViewModel.cs
@@ -1,9 +1,52 @@
-﻿using MvvmCross.Core.ViewModels;
+﻿using System.IO;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Plugins.PictureChooser;
 
 namespace IBM.Interview.ProjectA.Core.ViewModels
 {
     public class FinderViewModel : MvxViewModel
     {
+        private readonly IMvxPictureChooserTask _pictureChooser;
+        private byte[] _bytes;
+        private IMvxCommand _takePicture, _openGallery;
 
+        public byte[] Bytes
+        {
+            get { return _bytes; }
+            set
+            {
+                _bytes = value;
+                RaisePropertyChanged(() => Bytes);
+            }
+        }
+
+        public IMvxCommand TakePictureCommand => _takePicture ?? (_takePicture = new MvxCommand(TakePicture));
+        public IMvxCommand OpenGalleryCommand => _openGallery ?? (_openGallery = new MvxCommand(OpenGallery));
+
+        // constructor
+        public FinderViewModel(IMvxPictureChooserTask pictureChooser)
+        {
+            _pictureChooser = pictureChooser;
+        }
+
+        // open gallery
+        private void OpenGallery()
+        {
+            _pictureChooser.ChoosePictureFromLibrary(400, 95, OnPicture, () => { });
+        }
+
+        // take picture
+        private void TakePicture()
+        {
+            _pictureChooser.TakePicture(400, 95, OnPicture, () => { });
+        }
+
+        // handle image selection
+        private void OnPicture(Stream pictureStream)
+        {
+            var memoryStream = new MemoryStream();
+            pictureStream.CopyTo(memoryStream);
+            Bytes = memoryStream.ToArray();
+        }
     }
 }
---
+++ IBM.Interview.ProjectA/Bootstrap/PictureChooserPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+namespace IBM.Interview.ProjectA.Bootstrap
+{
+    public class PictureChooserPluginBootstrap
+        : MvxPluginBootstrapAction<MvvmCross.Plugins.PictureChooser.PluginLoader>
+    {
+    }
+}
